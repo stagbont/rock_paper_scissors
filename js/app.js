346a951c0cd942c03a1543f97c822ce5971e9d08
Bob Stanley Agbontor <stanleyagbontor63@gmail.com>
2022-07-15 11:22:01 +0000

    Done the with the playRound Function

--- js/app.js
+++ js/app.js
@@ -7,3 +7,31 @@ function computerPlay() {
     // return the random variable to the as an index of playable_Arr
     return playable_Arr[1];
 }
+
+let computerSelection = computerPlay();
+let score = 0;
+let playSelection;
+
+function playRound(playSelection, computerSelection) {
+    let player = playSelection.toLowerCase();
+    let computer = computerSelection.toLowerCase();
+
+    if ((computer === "rock" && player === "paper") ||
+        (computer === "paper" && player === "scissors") ||
+        (computer === "scissors" && player === "rock")) {
+            return console.log("You Won! " + player + " beats " + computer),
+            score++;
+    }
+
+    else if ((computer === "paper" && player === "rock") ||
+        (computer === "scissors" && player === "paper") ||
+        (computer === "rock" && player === "scissors")) {
+            return console.log("You Lose! " + computer + " beats " + player),
+            score--;
+        }
+    
+    else if (computer === player) {
+        return console.log("Tie"),
+        score;
+    }
+}
